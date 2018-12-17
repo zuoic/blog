c2867eddc04412fbf23208d20c129d0c4c7ce493
zuoic <178726336@qq.com>
2018-12-17 21:46:21 +0800

    注册未写完

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+
+const conn = mysql.createConnection({
+    host:'127.0.0.1',
+    database:'mysql_001',
+    user:'root',
+    password:'root'
+})
 //设置默认采用的模板引擎名称
 app.set('view engine','ejs')
 //设置模板页面的存放路径
@@ -26,6 +35,20 @@ app.get('/login',(req,res)=>{
 
 //注册新用户
 app.post('/register',(req,res)=>{
+    const body = req.body
+
+    //判断用户输入的数据是否完整
+if(body.username.trim().length<=0 || body.password.trim().length<=0 || body.nickname.trim().length<= 0){
+    return res.send({msg:'请填写完整的表单数据后再注册',status:501})
+}
+//查询用户是否重复
+const sql1 = 'select count(*) as count from blog_users where username=?'
+
+conn.query(sql1, body.username, (err,result)=>{
+    
+    if(err) return res.send({msg:'用户名查重失败!',status:502})
+    
+})
     res.send({msg:'ok',status:200})
 })
 
